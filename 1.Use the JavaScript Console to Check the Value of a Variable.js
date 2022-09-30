1e2993a391d873b01864a5ce5ceb930c09f8b8e8
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-30 18:58:15 -0400

    add solution to task 1.Use the JavaScript Console to Check the Value of a Variable.js

---
+++ 1.Use the JavaScript Console to Check the Value of a Variable.js
@@ -0,0 +1,9 @@
+//1.Use the JavaScript Console to Check the Value of a Variable.js
+
+//TASK: Use the console.log() method to print the value of the variable a where noted in the code.
+
+let a = 5;
+let b = 1;
+a++;
+let sumAB = a + b;
+console.log(a);
